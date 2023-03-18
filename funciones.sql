5e2b6d542d5b98d7da01826b03b3349a9c665cb4
Mathew <mateo12molina@gmail.com>
2023-03-18 14:02:05 -0600

    funciones

---
+++ funciones.sql
@@ -0,0 +1,6 @@
+ DELIMITER $$
+-- CREATE PROCEDURE contarProductosPorEstado(IN nestado varchar(25), OUT numero INT)
+-- BEGIN
+-- select count(id) into numero from productos where estado=nestado;
+-- END $$
+-- DELIMITER 
